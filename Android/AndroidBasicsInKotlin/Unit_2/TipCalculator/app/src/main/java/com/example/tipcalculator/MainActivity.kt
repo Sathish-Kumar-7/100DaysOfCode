0126931824f16b58974c05b532f77a91827ef1c9
Sathish-Kumar-7 <sathishkumarhs0705@gmail.com>
2022-05-24 11:55:53 +0530

    Day 3 -> icons themes styles

--- Android/AndroidBasicsInKotlin/Unit_2/TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ Android/AndroidBasicsInKotlin/Unit_2/TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.tipcalculator
 
+import android.content.Context
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tipcalculator.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import kotlin.math.ceil
@@ -36,10 +39,22 @@ class MainActivity : AppCompatActivity() {
         if (binding.roundUpSwitch.isChecked){
             return roundOfTip(tipAmount)
         }
+        binding.costEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
         return tipAmount
     }
 
     private fun roundOfTip(tipAmount: Double): Double {
         return ceil(tipAmount)
+    }
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
